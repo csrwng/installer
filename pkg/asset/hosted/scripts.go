242b8192d9f65364829d6653a31787eae35b857c
Cesar Wong <cewong@redhat.com>
2019-09-09 10:19:07 -0400

    Add expose script, fix render script

--- pkg/asset/hosted/scripts.go
+++ pkg/asset/hosted/scripts.go
@@ -5,6 +5,7 @@ import (
 	"path"
 
 	"github.com/openshift/installer/pkg/asset"
+	"github.com/openshift/installer/pkg/asset/installconfig"
 	"github.com/openshift/installer/pkg/asset/releaseimage"
 )
 
@@ -22,19 +23,24 @@ func (o *RenderingScripts) Name() string {
 func (o *RenderingScripts) Dependencies() []asset.Asset {
 	return []asset.Asset{
 		&releaseimage.Image{},
+		&installconfig.InstallConfig{},
 	}
 }
 
 // Generate creates the manifests needed to launch a hosted cluster
 func (o *RenderingScripts) Generate(dependencies asset.Parents) error {
 	releaseImage := &releaseimage.Image{}
-	dependencies.Get(releaseImage)
+	installConfig := &installconfig.InstallConfig{}
+	dependencies.Get(releaseImage, installConfig)
 
 	templateData := map[string]string{
-		"ReleaseImage": releaseImage.PullSpec,
+		"ReleaseImage":  releaseImage.PullSpec,
+		"BaseDomain":    installConfig.Config.BaseDomain,
+		"ClusterDomain": installConfig.Config.ClusterDomain(),
 	}
 	for _, f := range []string{
 		"render-operand-manifests.sh",
+		"expose-control-plane.sh",
 	} {
 		data, err := getFileContents(path.Join("scripts/hosted", fmt.Sprintf("%s.template", f)))
 		if err != nil {
